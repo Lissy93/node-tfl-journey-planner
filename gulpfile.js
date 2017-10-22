7da5645f5c2143df383646d4902655678cd632ad
Alicia Sykes <sykes.alicia@gmail.com>
2017-10-22 21:37:50 +0100

    New LiveScript code

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,18 @@ var gulp    = require('gulp');
 var watch   = require('gulp-watch');
 var minify  = require('gulp-uglify');
 var coffee  = require('gulp-coffee');
+var live    = require('gulp-livescript');
 var lint    = require('gulp-coffeelint');
+var gutil   = require('gutil');
 
 
 /* Lint, compile and minify CoffeeScript */
 gulp.task('build', function(){
-    return gulp.src('./index.coffee')
+    return gulp.src('./index.ls')
         .pipe(lint())
         .pipe(lint.reporter())
-        .pipe(coffee())
+        .pipe(live({bare: true})
+            .on('error', gutil.log))
         .pipe(minify())
         .pipe(gulp.dest('./'))
 });
